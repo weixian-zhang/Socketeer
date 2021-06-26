e808c9f4a4466a615033b2a469925dc404e8fa90
Code Norris <weixianzhang@outlook.com>
2021-06-26 14:51:27 +0800

    Use electron-forge webpack + typescript template

---
+++ socketeer/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+//https://dev.to/franamorim/tutorial-reminder-widget-with-electron-react-1hj9
+
+export default class App extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+}
